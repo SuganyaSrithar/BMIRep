f294e038024baf9f415fab85853395fc2715c575
Suganya S <Suganya.S@dsrc.in>
2018-12-11 16:50:54 +0530

    commit1

---
+++ bmiResult.java
@@ -0,0 +1,34 @@
+package com.mdhasan.bmicalculator;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author User
+ */
+public class bmiResult {
+    
+    
+    public String mbiCalculate(double weight, double height){
+        
+        double bmi= weight/height;
+        
+        if(bmi<18.5){
+            return "UNDERWEIGHT";
+        }else if(bmi>=18.5 && bmi<=23){
+            return "HEALTHY";
+        }else if(bmi>23 && bmi<=25){
+            return "AT RISK";
+        }else if(bmi>25){
+            return "OVERHEIGHT";
+        }else{
+            return "BMI cannot be determined!!!"
+        }
+    }
+    
+}
